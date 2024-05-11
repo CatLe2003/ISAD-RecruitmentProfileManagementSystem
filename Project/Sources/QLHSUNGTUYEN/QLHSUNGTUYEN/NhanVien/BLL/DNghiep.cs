2fb0ac8c1db8f44b33d020831edf67c9fc9ab733
OhMylOvY <127728388+OhMylOvY@users.noreply.github.com>
2024-05-11 11:54:31 +0700

    Update DNghiep.cs

--- Project/Sources/QLHSUNGTUYEN/QLHSUNGTUYEN/NhanVien/BLL/DNghiep.cs
+++ Project/Sources/QLHSUNGTUYEN/QLHSUNGTUYEN/NhanVien/BLL/DNghiep.cs
@@ -8,10 +8,10 @@ namespace QLHSUNGTUYEN.NhanVien.BLL
 {
     internal class DNghiep
     {
-        public int MaDN;
+        public int MaDN { get; set; }
 		//public int ID; 
 		public string Pass_word;
-		public string TenDN;
+		public string TenDN { get; set; }
 		public string MaSoThue;
 		public string NguoiDaiDien;
 		public string Diachi;
@@ -23,7 +23,16 @@ namespace QLHSUNGTUYEN.NhanVien.BLL
 		{
 
 		}
-
+        public List<DNghiep> DanhSachDoanhNghiep()
+        {
+            List<DNghiep> list = new List<DNghiep>();
+            DataTable all_DNgiep = new Database().SelectData("GetDoanhNghiep", null);
+            foreach (DataRow row in all_DNgiep.Rows)
+            {
+                list.Add(new DNghiep() { TenDN = row["TenDN"].ToString(), MaDN = int.Parse(row["MaDN"].ToString()) });
+            }
+            return list;
+        }
 		
     }
 
