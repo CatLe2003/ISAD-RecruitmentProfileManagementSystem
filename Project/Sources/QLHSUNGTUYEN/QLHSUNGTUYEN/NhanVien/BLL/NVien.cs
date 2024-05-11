e0960725a656b70bc7372eb1f391623c690e600f
OhMylOvY <127728388+OhMylOvY@users.noreply.github.com>
2024-05-11 11:59:03 +0700

    Update NVien.cs

--- Project/Sources/QLHSUNGTUYEN/QLHSUNGTUYEN/NhanVien/BLL/NVien.cs
+++ Project/Sources/QLHSUNGTUYEN/QLHSUNGTUYEN/NhanVien/BLL/NVien.cs
@@ -8,10 +8,10 @@ namespace QLHSUNGTUYEN.NhanVien.BLL
 {
     internal class NVien
     {
-        public int MaNV;
+        public int MaNV { get; set; }
         public int ID;
         public string Pass_word;
-        public string Hoten;
+        public string Hoten { get; set; }
         public string Gioitinh;
         public string Sdt;
         public string Diachi;
@@ -21,6 +21,19 @@ namespace QLHSUNGTUYEN.NhanVien.BLL
         public NVien()
         {
 
+        }
+        public string LayTenNhanVien(string manv) {
+            MaNV = int.Parse(manv);
+            List<CustomParameter> parameters = new List<CustomParameter>();
+            parameters.Add(new CustomParameter()
+            {
+                key = "@MaNV",
+                value = manv.ToString()
+            });
+            DataTable nhanvien = new Database().SelectData("GetTenNhanVien", parameters);
+            DataRow nhanvien_ = nhanvien.Rows[0];
+            Hoten = nhanvien_["Hoten"].ToString();
+            return Hoten.ToString();
         }
     }
 }
