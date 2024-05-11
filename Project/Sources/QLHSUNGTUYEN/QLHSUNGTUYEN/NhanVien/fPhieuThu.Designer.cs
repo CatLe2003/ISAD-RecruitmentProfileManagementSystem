e788ca822a68423ab72af9c327075aaa711df858
OhMylOvY <127728388+OhMylOvY@users.noreply.github.com>
2024-05-11 12:53:31 +0700

    Add files via upload

---
+++ Project/Sources/QLHSUNGTUYEN/QLHSUNGTUYEN/NhanVien/fPhieuThu.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace QLHSUNGTUYEN.NhanVien
+{
+    partial class fPhieuThu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbbdot = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.mtbNgayLap = new System.Windows.Forms.MaskedTextBox();
+            this.cbbhd = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbbDN = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cbbdot);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.mtbNgayLap);
+            this.panel1.Controls.Add(this.cbbhd);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.lblValue);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.cbbDN);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(610, 316);
+            this.panel1.TabIndex = 2;
+            // 
+            // cbbdot
+            // 
+            this.cbbdot.FormattingEnabled = true;
+            this.cbbdot.Location = new System.Drawing.Point(307, 239);
+            this.cbbdot.Name = "cbbdot";
+            this.cbbdot.Size = new System.Drawing.Size(281, 21);
+            this.cbbdot.TabIndex = 53;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(307, 189);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 20);
+            this.textBox1.TabIndex = 52;
+            // 
+            // mtbNgayLap
+            // 
+            this.mtbNgayLap.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbNgayLap.Location = new System.Drawing.Point(307, 138);
+            this.mtbNgayLap.Margin = new System.Windows.Forms.Padding(2);
+            this.mtbNgayLap.Mask = "00/00/0000";
+            this.mtbNgayLap.Name = "mtbNgayLap";
+            this.mtbNgayLap.Size = new System.Drawing.Size(281, 26);
+            this.mtbNgayLap.TabIndex = 51;
+            this.mtbNgayLap.ValidatingType = typeof(System.DateTime);
+            // 
+            // cbbhd
+            // 
+            this.cbbhd.FormattingEnabled = true;
+            this.cbbhd.Location = new System.Drawing.Point(307, 96);
+            this.cbbhd.MaxDropDownItems = 100;
+            this.cbbhd.Name = "cbbhd";
+            this.cbbhd.Size = new System.Drawing.Size(281, 21);
+            this.cbbhd.TabIndex = 50;
+            this.cbbhd.SelectionChangeCommitted += new System.EventHandler(this.cbbhd_SelectionChangeCommitted);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(20, 97);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(104, 20);
+            this.label8.TabIndex = 49;
+            this.label8.Text = "Mã Hợp Đồng";
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValue.Location = new System.Drawing.Point(431, 13);
+            this.lblValue.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(38, 20);
+            this.lblValue.TabIndex = 48;
+            this.lblValue.Text = "kiên";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(357, 13);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 20);
+            this.label7.TabIndex = 47;
+            this.label7.Text = "Xin chào ";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(307, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 46;
+            this.button2.Text = "Lưu";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 45;
+            this.button1.Text = "Hủy";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // cbbDN
+            // 
+            this.cbbDN.FormattingEnabled = true;
+            this.cbbDN.Location = new System.Drawing.Point(307, 53);
+            this.cbbDN.MaxDropDownItems = 100;
+            this.cbbDN.Name = "cbbDN";
+            this.cbbDN.Size = new System.Drawing.Size(281, 21);
+            this.cbbDN.TabIndex = 44;
+            this.cbbDN.SelectionChangeCommitted += new System.EventHandler(this.cbbDN_SelectionChangeCommitted);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(542, 187);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 20);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Đồng";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(554, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(20, 54);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Doanh Nghiệp";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 188);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Số Tiền:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 138);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Ngày Thu:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 240);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Đợt thu tiền:";
+            // 
+            // fPhieuThu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 340);
+            this.Controls.Add(this.panel1);
+            this.Name = "fPhieuThu";
+            this.Text = "fPhieuThu";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cbbDN;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.ComboBox cbbhd;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.MaskedTextBox mtbNgayLap;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox cbbdot;
+    }
+}
---
+++ Project/Sources/QLHSUNGTUYEN/QLHSUNGTUYEN/NhanVien/fPhieuThu.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+
+namespace QLHSUNGTUYEN.NhanVien
+{
+    public partial class fPhieuThu : Form
+    {
+        private string manv = "";
+        private PhieuThu thu = new PhieuThu();
+        public fPhieuThu(string manv)
+        {
+            this.manv = manv;
+            InitializeComponent();
+            fPhieuThu_Load();
+        }
+
+        private void fPhieuThu_Load()
+        {
+            cbbDN.Enabled = true;
+            cbbdot.Enabled = true;
+            List<DNghiep> list = new DNghiep().DanhSachDoanhNghiep();
+
+            cbbDN.DataSource = list;
+            cbbDN.ValueMember = "MaDN";
+            cbbDN.DisplayMember = "TenDN";
+
+            lblValue.Text = new NhanVien().LayTenNhanVien(manv);
+            mtbNgayLap.Text = DateTime.Now.ToString("dd/MM/yyyy");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HopDongg hd = cbbhd.SelectedItem as HopDongg;
+
+            // Kiểm tra xem đã chọn hợp đồng và số đợt thanh toán chưa
+            if (hd != null && cbbdot.SelectedItem is string soDotTT)
+            {
+                // Chuyển đổi mã nhân viên từ chuỗi sang số nguyên
+                if (int.TryParse(this.manv, out int maNV))
+                {
+                    // Chuyển đổi ngày từ chuỗi sang đối tượng DateTime
+                    if (DateTime.TryParseExact(mtbNgayLap.Text, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime ngayThu))
+                    {
+                        // Chuyển đổi mã hợp đồng từ chuỗi sang số nguyên
+                        if (int.TryParse(hd.MaHDong, out int maHDong))
+                        {
+                            // Chuyển đổi số tiền từ chuỗi sang số nguyên
+                            if (int.TryParse(textBox1.Text, out int soTien))
+                            {
+                                // Chuyển đổi số đợt thanh toán từ chuỗi sang số nguyên
+                                if (int.TryParse(soDotTT, out int dotThu))
+                                {
+                                    thu.MaNV = maNV;
+                                    thu.NgayThu = ngayThu;
+                                    thu.MaHDong = maHDong;
+                                    thu.SoTien = soTien;
+                                    thu.DotThu = dotThu;
+                                    bool check = thu.LapPhieuThu();
+                                    if (check)
+                                    {
+                                        MessageBox.Show("Lập phiếu thu thành công!");
+                                        this.Dispose();
+                                    }
+                                    // Đã gán giá trị thành công, bạn có thể thực hiện thêm xử lý ở đây
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Số đợt thanh toán không hợp lệ!");
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Số tiền không hợp lệ!");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Mã hợp đồng không hợp lệ!");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ngày thu không hợp lệ! Vui lòng nhập theo định dạng dd/MM/yyyy.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Mã nhân viên không hợp lệ!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn hợp đồng và số đợt thanh toán!");
+            }
+        }
+        private void cbbhd_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            HopDongg selectedItem = cbbhd.SelectedItem as HopDongg;
+            if (selectedItem != null)
+            {
+                int i = thu.TimDotThuLonNhat(selectedItem.MaHDong) + 1;
+                int soDotTT = selectedItem.SoDotTT;
+                cbbdot.Items.Clear();
+                for (; i <= soDotTT; i++)
+                {
+                    cbbdot.Items.Add(i.ToString());
+                }
+            }
+        }
+
+        private void cbbDN_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            DNghiep selectedItem = cbbDN.SelectedItem as DNghiep;
+            if (selectedItem != null)
+            {
+                List<HopDongg> list = new HopDongg().DanhSachHopDong(selectedItem.MaDN.ToString());
+
+                cbbhd.DataSource = list;
+                cbbhd.ValueMember = "MaHDong";
+                cbbhd.DisplayMember = "MaHDong";
+            }
+        }
+    }
+}
