e6fc5ab8866e486e9e553681e4e429f7dc7b5455
OhMylOvY <127728388+OhMylOvY@users.noreply.github.com>
2024-05-11 12:15:10 +0700

    Add files via upload

---
+++ Project/Sources/QLHSUNGTUYEN/QLHSUNGTUYEN/NhanVien/BLL/PhieuThu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.AxHost;
+using System.Windows.Forms;
+
+namespace QLHSUNGTUYEN
+{
+    internal class PhieuThu
+    {
+        public int MaNV { get; set; }
+        public int DotThu { get; set; }
+        public DateTime NgayThu { get; set; }
+        public int SoTien { get; set; }
+        public int MaHDong { get; set; }
+
+        public bool LapPhieuThu() {
+            List<CustomParameter> parameters = new List<CustomParameter>();
+            parameters.Add(new CustomParameter()
+            {
+                key = "@MaNV",
+                value = MaNV.ToString()
+            });
+            parameters.Add(new CustomParameter()
+            {
+                key = "@DotThu",
+                value = DotThu.ToString()
+            });
+            parameters.Add(new CustomParameter()
+            {
+                key = "@NgayThu",
+                value = NgayThu.ToString("dd/MM/yyyy")
+            });
+            parameters.Add(new CustomParameter()
+            {
+                key = "@SoTien",
+                value = SoTien.ToString()
+            });
+            parameters.Add(new CustomParameter()
+            {
+                key = "@MaHDong",
+                value = MaHDong.ToString()
+            });
+            var res = new Database().ExeCute("LapPhieuThu", parameters);
+            if (res == 1) // nếu thực thi thành công
+            {
+                return true;
+            }
+            else // nếu không thành công
+            {
+                return false;
+            }
+        }
+    }
+}
